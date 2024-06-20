55cbf8564c09d0d73e578b5323756ec0c0849667
Gomah Haj taha <gomahhajtaha@gomahs-laptop.speedport.ip>
2024-06-20 08:14:34 +0200

    changes in index file

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -48,7 +48,7 @@ const ContactUs = ({setSelectedPage}: Props) => {
           </HText>
           <p className="my-5">
             Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
-            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulle
             adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
           </p>
         </motion.div>
